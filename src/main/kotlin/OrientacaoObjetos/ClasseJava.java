e18bbe163888ce9979aa564135b37358443ee41c
Luis Eduardo <vsluiseduardo01@gmail.com>
2023-02-07 00:07:50 -0300

    Getter, Setter e field

---
+++ src/main/kotlin/OrientacaoObjetos/ClasseJava.java
@@ -0,0 +1,11 @@
+package OrientacaoObjetos;
+
+public class ClasseJava {
+    public static void main(String[] args) {
+        Planeta p = new Planeta();
+        p.getNome();
+
+        Animal animal = new Animal("cachorro");
+        animal.getFala();
+    }
+}
